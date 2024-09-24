4c3fc5e3d9dea3bac91a746d6845763053f88c6c
erick-sts <erick.sts.eo@gmail.com>
2024-09-23 22:16:16 -0300

    class 23-09

--- src/app/products/edit/[slug]/page.tsx
+++ src/app/products/edit/[slug]/page.tsx
@@ -1,92 +1,12 @@
-'use client'
-import Layout from "@/components/UI/organisms/Layout";
-import { IProduct } from "@/interface/IProducts";
-import { ProductEditValidater } from "@/validators/ProductEditValidator";
-import {
-  Box,
-  Button,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
-import { useFormik } from "formik";
+"use client";
+import EditTemplate from "@/components/templates/products/EditTemplate";
 
 interface ProductEditProps {
   params: { slug: string };
 }
 
 const ProductEdit: React.FC<ProductEditProps> = ({ params }) => {
-  const formik = useFormik<IProduct>({
-    initialValues: {
-      brand: "",
-      description: "",
-      flavor: "",
-      weight: 0,
-      value: 0,
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-    validationSchema: ProductEditValidater,
-  });
-
-  const { handleSubmit, values, handleChange, setFieldValue } = formik;
-
-  return (
-    <Layout>
-      <Box component="form" onSubmit={handleSubmit}>
-        <TextField
-          name="description"
-          label="Descrição"
-          fullWidth
-          value={values.description}
-          onChange={handleChange}
-        />
-        <TextField
-          name="brand"
-          label="Marca"
-          fullWidth
-          value={values.brand}
-          onChange={handleChange}
-        />
-        <TextField
-          name="value"
-          label="Valor"
-          fullWidth
-          value={values.value}
-          onChange={handleChange}
-        />
-        <TextField
-          name="weight"
-          label="Peso (g)"
-          fullWidth
-          value={values.weight}
-          onChange={handleChange}
-        />
-
-        <Select
-          name="flavor"
-          label="Sabor"
-          fullWidth
-          value={values.flavor}
-          onChange={(e) => setFieldValue("flavor", e.target.value)}
-        >
-          <MenuItem value="abacaxi_ao_vinho">Abacaxi ao vinho</MenuItem>
-          <MenuItem value="frutas_vermelhas">Frutas Vermelhas</MenuItem>
-          <MenuItem value="menta">Menta</MenuItem>
-          <MenuItem value="">-- NÃO INFORMADO --</MenuItem>
-        </Select>
-
-        <Button variant="outlined" color="secondary">
-          Cancelar
-        </Button>
-        <Button variant="contained" color="primary" type="submit">
-          Atualizar
-        </Button>
-      </Box>
-    </Layout>
-  );
+  return <EditTemplate />;
 };
 
 export default ProductEdit;
--- src/validators/ProductEditValidator.ts
+++ src/validators/ProductEditValidator.ts
@@ -1,11 +1,13 @@
-import * as Yup from "yup"
+import * as Yup from "yup";
 
-export const ProductEditValidater = () =>{
-    return Yup.object().shape({
-        description: Yup.string().required().min(3).max(100),
-        brand: Yup.string().required().max(80),
-        value: Yup.number().min(0.01).required(),
-        weight: Yup.number().min(0.01),
-        flavor: Yup.string().max(50),
-    })
-}
+export const ProductEditValidater = () => {
+  return Yup.object().shape({
+    description: Yup.string().required("Campo Obrigatório").min(3).max(100),
+    brand: Yup.string().required("Campo Obrigatório").max(80),
+    value: Yup.number()
+      .min(0.01, "Campo deve ter pelo nenos ${min}")
+      .required(),
+    weight: Yup.number().min(0.01),
+    flavor: Yup.string().max(50),
+  });
+};
